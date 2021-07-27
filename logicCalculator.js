b3891905da9b576b20ce355dd34c503b64898114
Jairo Alzate <jairo.alzate@siesa.com>
2021-07-27 11:09:01 -0500

    Add while to Calculator

---
+++ logicCalculator.js
@@ -0,0 +1,38 @@
+// menu
+let op = document.getElementById("op");
+
+const operaciones = () => {
+  let op = prompt(
+    "Escribe: \n 1 - Suma \n 2 - Resta \n 3 - Multiplicación \n 4 - División"
+  );
+
+  while (op !== "1" && op !== "2" && op !== "3" && op !== "4") {
+    alert("Favor ingresar una opcion valida de 1 a 4");
+    op = prompt(
+      "Escribe: \n 1 - Suma \n 2 - Resta \n 3 - Multiplicación \n 4 - División"
+    );
+  } 
+
+  let num1 = parseFloat(prompt("Ingrese numero 1"));
+  let num2 = parseFloat(prompt("Ingrese numero 2"));
+  let resultado = 0;
+  if (op === "1") {
+    resultado = num1 + num2;
+  } else if (op === "2") {
+    resultado = num1 - num2;
+  } else if (op === "3") {
+    resultado = num1 * num2;
+  } else {
+    while (num2 === 0) {
+      alert("No se puede dividir por 0");
+      num2 = parseFloat(prompt("Ingrese numero 2"));
+    } 
+    resultado = num1 / num2;
+  }
+  alert("El resultado es: " + resultado);
+};
+
+//eventos
+op.onclick = function () {
+  operaciones();
+};
