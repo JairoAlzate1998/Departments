23b52925b5fe86a40789d41563a509ba216de1cb
Jairo Alzate <jairo.alzate@siesa.com>
2021-07-27 11:42:43 -0500

    Add exit option

--- logicCalculator.js
+++ logicCalculator.js
@@ -3,15 +3,22 @@ let op = document.getElementById("op");
 
 const operaciones = () => {
   let op = prompt(
-    "Escribe: \n 1 - Suma \n 2 - Resta \n 3 - Multiplicación \n 4 - División"
+    "Escribe: \n 1 - Suma \n 2 - Resta \n 3 - Multiplicación \n 4 - División \n 5 - Salir"
   );
 
-  while (op !== "1" && op !== "2" && op !== "3" && op !== "4") {
-    alert("Favor ingresar una opcion valida de 1 a 4");
+  if (op == 5) {
+    return alert("Hasta luego");
+  }
+
+  while (op !== "1" && op !== "2" && op !== "3" && op !== "4" && op !== 5) {
+    alert("Favor ingresar una opcion valida de 1 a 5");
     op = prompt(
-      "Escribe: \n 1 - Suma \n 2 - Resta \n 3 - Multiplicación \n 4 - División"
+      "Escribe: \n 1 - Suma \n 2 - Resta \n 3 - Multiplicación \n 4 - División \n 5 - Salir"
     );
-  } 
+    if (op == 5) {
+      return alert("Hasta luego");
+    }
+  }
 
   let num1 = parseFloat(prompt("Ingrese numero 1"));
   let num2 = parseFloat(prompt("Ingrese numero 2"));
@@ -26,7 +33,7 @@ const operaciones = () => {
     while (num2 === 0) {
       alert("No se puede dividir por 0");
       num2 = parseFloat(prompt("Ingrese numero 2"));
-    } 
+    }
     resultado = num1 / num2;
   }
   alert("El resultado es: " + resultado);
--- logicDepartments.js
+++ logicDepartments.js
@@ -1,39 +1,7 @@
 // ingresar un pueblo o una ciudad y debe responder la app con el departamento
-alert(
-  "Ingresa el nombre de la ciudad. Ciudades disponibles en Boyaca: Duitama, sogamoso, tunja, paipa"
-);
-alert(
-  "Ingresa el nombre de la ciudad. Ciudades disponibles en Cundinamarca: ubate, tausa, simijaca, sibate"
-);
-alert(
-  "Ingresa el nombre de la ciudad. Ciudades disponibles en Valle del Cauca: alcala, cali, florida, palmira"
-);
-alert(
-  "Ingresa el nombre de la ciudad. Ciudades disponibles en Atlantico: barranquilla, polo nuevo, soledad, manati"
-);
+let ciudad = document.getElementById("ciudad");
 
-let municipio = prompt("Ingresa el nombre de la ciudad");
-municipio = municipio.toLowerCase();
-
-while (
-  municipio !== "duitama" &&
-  municipio !== "sogamoso" &&
-  municipio !== "tunja" &&
-  municipio !== "paipa" &&
-  municipio !== "ubate" &&
-  municipio !== "tausa" &&
-  municipio !== "simijaca" &&
-  municipio !== "sibate" &&
-  municipio !== "alcala" &&
-  municipio !== "cali" &&
-  municipio !== "florida" &&
-  municipio !== "palmira" &&
-  municipio !== "barranquilla" &&
-  municipio !== "polo nuevo" &&
-  municipio !== "soledad" &&
-  municipio !== "manati"
-) {
-  alert("Ingresa una ciudad valida");
+const operaciones = () => {
   alert(
     "Ingresa el nombre de la ciudad. Ciudades disponibles en Boyaca: Duitama, sogamoso, tunja, paipa"
   );
@@ -46,36 +14,86 @@ while (
   alert(
     "Ingresa el nombre de la ciudad. Ciudades disponibles en Atlantico: barranquilla, polo nuevo, soledad, manati"
   );
-  municipio = prompt("Ingresa el nombre de la ciudad");
-  municipio = municipio.toLowerCase();
-}
 
-if (
-  municipio == "duitama" ||
-  municipio == "sogamoso" ||
-  municipio == "tunja" ||
-  municipio == "paipa"
-) {
-  alert(municipio + " pertenece al departamento Boyaca");
-} else if (
-  municipio == "ubate" ||
-  municipio == "tausa" ||
-  municipio == "simijaca" ||
-  municipio == "sibate"
-) {
-  alert(municipio + " pertenece al departamento Cundinamarca");
-} else if (
-  municipio == "alcala" ||
-  municipio == "cali" ||
-  municipio == "florida" ||
-  municipio == "palmira"
-) {
-  alert(municipio + " pertenece al departamento Valle del Cauca");
-} else if (
-  municipio == "barranquilla" ||
-  municipio == "polo nuevo" ||
-  municipio == "soledad" ||
-  municipio == "manati"
-) {
-  alert(municipio + " pertenece al departamento Valle del Cauca");
-}
+  let municipio = prompt(
+    "Ingresa el nombre de la ciudad o ingrese salir para terminar"
+  );
+  municipio = municipio.toLowerCase();
+
+  if (municipio === "salir") {
+    return alert("Hasta luego");
+  }
+
+  while (
+    municipio !== "duitama" &&
+    municipio !== "sogamoso" &&
+    municipio !== "tunja" &&
+    municipio !== "paipa" &&
+    municipio !== "ubate" &&
+    municipio !== "tausa" &&
+    municipio !== "simijaca" &&
+    municipio !== "sibate" &&
+    municipio !== "alcala" &&
+    municipio !== "cali" &&
+    municipio !== "florida" &&
+    municipio !== "palmira" &&
+    municipio !== "barranquilla" &&
+    municipio !== "polo nuevo" &&
+    municipio !== "soledad" &&
+    municipio !== "manati"
+  ) {
+    alert("Ingresa una ciudad valida");
+    alert(
+      "Ingresa el nombre de la ciudad. Ciudades disponibles en Boyaca: Duitama, sogamoso, tunja, paipa"
+    );
+    alert(
+      "Ingresa el nombre de la ciudad. Ciudades disponibles en Cundinamarca: ubate, tausa, simijaca, sibate"
+    );
+    alert(
+      "Ingresa el nombre de la ciudad. Ciudades disponibles en Valle del Cauca: alcala, cali, florida, palmira"
+    );
+    alert(
+      "Ingresa el nombre de la ciudad. Ciudades disponibles en Atlantico: barranquilla, polo nuevo, soledad, manati"
+    );
+    municipio = prompt(
+      "Ingresa el nombre de la ciudad o ingrese salir para terminar"
+    );
+    municipio = municipio.toLowerCase();
+    if (municipio === "salir") {
+      return alert("Hasta luego");
+    }
+  }
+
+  if (
+    municipio == "duitama" ||
+    municipio == "sogamoso" ||
+    municipio == "tunja" ||
+    municipio == "paipa"
+  ) {
+    alert(municipio + " pertenece al departamento Boyaca");
+  } else if (
+    municipio == "ubate" ||
+    municipio == "tausa" ||
+    municipio == "simijaca" ||
+    municipio == "sibate"
+  ) {
+    alert(municipio + " pertenece al departamento Cundinamarca");
+  } else if (
+    municipio == "alcala" ||
+    municipio == "cali" ||
+    municipio == "florida" ||
+    municipio == "palmira"
+  ) {
+    alert(municipio + " pertenece al departamento Valle del Cauca");
+  } else if (
+    municipio == "barranquilla" ||
+    municipio == "polo nuevo" ||
+    municipio == "soledad" ||
+    municipio == "manati"
+  ) {
+    alert(municipio + " pertenece al departamento Valle del Cauca");
+  }
+};
+ciudad.onclick = function () {
+  operaciones();
+};
